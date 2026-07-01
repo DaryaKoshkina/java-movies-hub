edbb5241cc713ef6d57e0261b0e2954b658bc80d
Darya <daria.koshkina228@yandex.ru>
2026-07-01 21:05:18 +0500

    feat: добавила класс кино

--- src/main/ru/practicum/moviehub/model/Movie.java
+++ src/main/ru/practicum/moviehub/model/Movie.java
@@ -1,5 +1,25 @@
 package ru.practicum.moviehub.model;
 
 public class Movie {
+    private final Long id;
+    private final String name;
+    private final Integer  year;
 
+    public Movie(Long id, Integer year, String name) {
+        this.id = id;
+        this.year = year;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
